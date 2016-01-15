0fd23200c43a7d8a116d3561ac4af7129f350c02
phi <phi1618jp@gmail.com>
2016-01-15 20:54:56 +0900

    add demo

---
+++ demos/colorifyjs_demo/main.js
@@ -0,0 +1,12 @@
+/*
+ * runstant
+ */
+
+window.onload = function() {
+  colorify({
+    container: 'colorify-gradient-color',
+    accuracy: 10,
+    gradient: true,
+    gradientDirection: 'to bottom right'
+  });
+};
